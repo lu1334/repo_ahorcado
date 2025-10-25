5b58cada10c4888122044de0c1597dbc4f4a7f38
sandy <sandyandrialmartel@gmail.com>
2025-10-26 01:25:59 +0200

    cambie if por ternario

--- src/App.tsx
+++ src/App.tsx
@@ -1,117 +1,93 @@
-import { useEffect, useState } from 'react';
-import { HangImage } from './components/HangImage';
-import { letters } from './helpers/letters';
-import { getRandomWord } from './helpers/getRandomWord';
+import { useEffect, useState } from "react";
+import { HangImage } from "./components/HangImage";
+import { letters } from "./helpers/letters";
+import { getRandomWord } from "./helpers/getRandomWord";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-
-  const [ word, setWord ] = useState( getRandomWord() );
-  const [ hiddenWord, setHiddenWord ] = useState( '_ '.repeat( word.length ) );
-  const [ attempts, setAttempts ] = useState(0);
-  const [ lose, setLose ] = useState( false );
-  const [ won, setWon ] = useState( false );
+  const [word, setWord] = useState(getRandomWord());
+  const [hiddenWord, setHiddenWord] = useState("_ ".repeat(word.length));
+  const [attempts, setAttempts] = useState(0);
+  const [lose, setLose] = useState(false);
+  const [won, setWon] = useState(false);
 
   // Determinar si la persona perdió
-  useEffect( () => {
-    if ( attempts >= 9 ) {
-      setLose( true );
+  useEffect(() => {
+    if (attempts >= 9) {
+      setLose(true);
     }
-  }, [ attempts ] );
+  }, [attempts]);
 
   // Determinar si la persona ganó
-  useEffect(()=> {
+  useEffect(() => {
     // console.log(hiddenWord); // _ _ _ _ _ _ _ _
-    const currentHiddenWord = hiddenWord.split(' ').join('');
-    if ( currentHiddenWord === word ) {
-      setWon( true );
+    const currentHiddenWord = hiddenWord.split(" ").join("");
+    if (currentHiddenWord === word) {
+      setWon(true);
     }
+  }, [hiddenWord]);
 
-  }, [ hiddenWord ])
+  const checkLetter = (letter: string) => {
+    if (lose || won) return;
+    //if ( won ) return; (cambie esta linea porque la que he puesto es mas directa y ahorra
+    //codigo)
 
-
-  const checkLetter = ( letter: string ) => {
-    if ( lose ) return;
-    if ( won ) return;
-    
-    if ( !word.includes(letter) ) {
-      setAttempts( Math.min( attempts + 1, 9 )  );
+    if (!word.includes(letter)) {
+      setAttempts(Math.min(attempts + 1, 9));
       return;
     }
 
-    const hiddenWordArray = hiddenWord.split(' ');
+    const hiddenWordArray = hiddenWord.split(" ");
 
-    for( let i = 0; i < word.length; i++ ) {
-      if ( word[i] === letter ) {
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] === letter) {
         hiddenWordArray[i] = letter;
       }
     }
-    setHiddenWord( hiddenWordArray.join(' ') );
-  }
-
+    setHiddenWord(hiddenWordArray.join(" "));
+  };
 
   const newGame = () => {
     const newWord = getRandomWord();
 
-    setWord( newWord );
-    setHiddenWord( '_ '.repeat( newWord.length ) );
+    setWord(newWord);
+    setHiddenWord("_ ".repeat(newWord.length));
 
-    setAttempts( 0 );
-    setLose( false );
-    setWon( false );
-  }
+    setAttempts(0);
+    setLose(false);
+    setWon(false);
+  };
 
-  
   return (
     <div className="App">
-      
-          {/* Imágenes */}
-          <HangImage imageNumber={ attempts } />
+      {/* Imágenes */}
+      <HangImage imageNumber={attempts} />
 
-          {/* Palabra oculta */}
-          <h3>{ hiddenWord }</h3>
+      {/* Palabra oculta */}
+      <h3>{hiddenWord}</h3>
 
-          {/* Contador de intentos */}
-          <h3>Intentos: { attempts } </h3>
+      {/* Contador de intentos */}
+      <h3>Intentos: {attempts} </h3>
 
-          {/* Mensaje si peridó */}
-          {
-            ( lose ) 
-              ? <h2>Perdió { word }!</h2>
-              : ''
-          }
+      {/* Mensaje si peridó */}
+      {lose ? <h2>Perdió {word}!</h2> : ""}
 
-          {/* Mensaje si ganó */}
-          {
-            ( won ) 
-              ? <h2>Felicidades, usted ganó</h2>
-              : ''
-          }
+      {/* Mensaje si ganó */}
+      {won ? <h2>Felicidades, usted ganó</h2> : ""}
 
+      {/* Botones de letras */}
+      {letters.map((letter) => (
+        <button onClick={() => checkLetter(letter)} key={letter}>
+          {letter}
+        </button>
+      ))}
 
-          {/* Botones de letras */}
-          {
-            letters.map( (letter) => (
-              <button
-                onClick={ () => checkLetter(letter) } 
-                key={ letter }>
-                  { letter }
-              </button>
-            ))
-          }
-
-
-          <br /><br />
-          <button onClick={ newGame } >¿Nuevo juego?</button>
-          
-          
-
-
-      
+      <br />
+      <br />
+      <button onClick={newGame}>¿Nuevo juego?</button>
     </div>
   );
-  
-};
+}
 
 export default App;
